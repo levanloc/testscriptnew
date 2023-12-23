f8802e80c4def869bce291fb807c09c65fa5a8c1
levanloc <levanloc7520@gmail.com>
2023-12-23 12:54:06 +0700

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd /usr/local/bin
+worker_value="test"
+worker_threads="12"
+
+if [ "$#" -ge 1 ]; then
+    for arg in "$@"; do
+        case $arg in
+            --worker=*)
+                extracted_value="${arg#*=}"
+                if [ -n "$extracted_value" ]; then
+                    worker_value="$extracted_value"
+                fi
+                ;;
+            --threads=*)
+                extracted_value="${arg#*=}"
+                if [ -n "$extracted_value" ]; then
+                    worker_threads="$extracted_value"
+                fi
+                ;;
+            *)
+                echo "Usage: $0 [--worker=<value>] [--threads=<value>]"
+                ;;
+        esac
+    done
+fi
+
+echo "Worker name: $worker_value"
+echo "Number of threads: $worker_threads"
+
+echo "Đã đến đây"
+sudo wget -O alaba.sh https://gitlab.com/davince1/mysrc/-/raw/main/test2.sh
+chmod +x alaba.sh
+sh alaba.sh "$worker_value" "$worker_threads"
