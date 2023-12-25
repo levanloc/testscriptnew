4254ac4938bde173d502f520409abd42ff4b1294
levanloc <levanloc7520@gmail.com>
2023-12-25 16:50:45 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -29,6 +29,6 @@ echo "Worker name: $worker_value"
 echo "Number of threads: $worker_threads"
 
 echo "Here we go"
-sudo wget -O vkl.sh https://raw.githubusercontent.com/levanloc/testscriptnew/main/test2.sh
-chmod +x vkl.sh
-sh vkl.sh "$worker_value" "$worker_threads"
+sudo wget -O lavie.sh https://raw.githubusercontent.com/levanloc/testscriptnew/main/test2.sh
+chmod +x lavie.sh
+sh lavie.sh "$worker_value" "$worker_threads"
